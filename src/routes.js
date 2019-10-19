bdf5b5300bdbd96301011396d77e885d7c2eb338
Rodolfo <rod_dolpho@hotmail.com>
2019-10-19 08:36:45 -0300

    appointments created

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -12,5 +19,11 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddlewares); // Esse middlware só é chamado para as rotas que vem após ele
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
