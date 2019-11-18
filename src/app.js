167a7579b7b6972ebb72a9824e03392b902d0e53
Rodolfo <rod_dolpho@hotmail.com>
2019-11-18 20:35:24 -0300

    fixed some aspects in session controller

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import path from 'path';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
-
 import './database';
 
 class App {
@@ -22,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
